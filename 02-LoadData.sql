5786e9c700a4b9772b33f3a2ab4f88016344d0d1
derosej <jason.derose@experient-inc.com>
2017-04-13 12:01:38 -0400

    Initial slideshow

---
+++ 02-LoadData.sql
@@ -0,0 +1,25 @@
+/*
+
+Replace <Your-Access-Key-ID> and <Your-Secret-Access-Key> with your own AWS account credentials. The segment of the credentials string that is enclosed in single quotes must not contain any spaces or line breaks.
+
+*/
+
+copy customer from 's3://awssampledbuswest2/ssbgz/customer' 
+credentials 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>' 
+gzip compupdate off region 'us-west-2';
+
+copy dwdate from 's3://awssampledbuswest2/ssbgz/dwdate' 
+credentials 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>' 
+gzip compupdate off region 'us-west-2';
+
+copy lineorder from 's3://awssampledbuswest2/ssbgz/lineorder' 
+credentials 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>'
+gzip compupdate off region 'us-west-2';
+
+copy part from 's3://awssampledbuswest2/ssbgz/part' 
+credentials 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>'
+gzip compupdate off region 'us-west-2';
+
+copy supplier from 's3://awssampledbuswest2/ssbgz/supplier' 
+credentials 'aws_access_key_id=<Your-Access-Key-ID>;aws_secret_access_key=<Your-Secret-Access-Key>'
+gzip compupdate off region 'us-west-2';
---
+++ 03-VerifyRowCounts.sql
@@ -0,0 +1,14 @@
+/*
+LINEORDER	600,037,902
+PART		1,400,000
+CUSTOMER	3,000,000
+SUPPLIER	1,000,000
+DWDATE		2,556
+*/
+
+
+select count(*) from LINEORDER;
+select count(*) from PART;
+select count(*) from  CUSTOMER;
+select count(*) from  SUPPLIER;
+select count(*) from  DWDATE;
